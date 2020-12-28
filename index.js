675256c7736f022eedc2821b6701cd2e4b5a2908
Dmg5th <dmgould310@gmail.com>
2020-12-28 13:28:01 -0500

    commit tree 0a407f42c29b03e4497c956e1bbee12998bd94a1

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendDriver(name){
+    drivers.push(`${name}`); 
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(`${name}`); 
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop(`${name}`); 
+}
+
+function destructivelyRemoveFirstDriver(){
+    drivers.shift(`${name}`);
+}
+
+function appendDriver(name){
+    let newarr = []
+    newarr.push( ...drivers,`${name}`); 
+    return newarr; 
+}
+
+function prependDriver(name){
+    let newarr = [];
+    newarr.unshift(`${name}`,...drivers)
+    return newarr 
+}
+
+function removeLastDriver(){
+    let newarr = drivers.slice(0, drivers.length-1);
+    return newarr
+}
+
+function removeFirstDriver(){
+    let newarr = drivers.slice(1);
+    return newarr
+}
